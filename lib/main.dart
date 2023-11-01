fb110a3d44ba58b860b626402896724b52e623ba
ghina2703 <ghina2703@gmail.com>
2023-11-01 14:39:34 +0700

    Tugas 6 Selesai :D

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        appBarTheme: AppBarTheme(backgroundColor: Colors.indigo),
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo),
         useMaterial3: true,
       ),
